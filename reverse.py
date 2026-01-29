d9d4c780820d5473ab0c5402d7a021b53e9ef76f
B.HARSHAVARDHAN <saiharshavardhan2003@gmail.com>
2026-01-29 16:04:45 +0530

    perfect number updated

--- reverse.py
+++ reverse.py
@@ -16,3 +16,13 @@ print(reverse(123))
 print(isPalindrome(123))
 print(reverse(121))
 print(isPalindrome(121))
+
+
+def getPalindromes(start,end):
+    res=""
+    for i in range(1,end+1):
+        if isPalindrome(i):
+            res=res+str(i)+","
+    return res
+print(getPalindromes(1,10000))
+    
